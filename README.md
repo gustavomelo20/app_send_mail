7098cc687cdb4e4a0e3f264f74549b160c79a576
gustavomelo20 <infogustavomelo@gmail.com>
2021-03-25 20:47:59 -0300

    Add PHPMailer

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # app_send_mail
+<h1> Sistemas de envio de email<h1>
--- processa_envio.php
+++ processa_envio.php
@@ -1,23 +1,33 @@
 <?php
 
-class Mensagem {
-     private $para = null;
-     private $assunto = null;
-     private $mensagem = null;
+    require "./bibliotecas/PHPMailer/Exception.php";
+    require "./bibliotecas/PHPMailer/OAuth.php";
+    require "./bibliotecas/PHPMailer/PHPMailer.php";
+    require "./bibliotecas/PHPMailer/POP3.php";
+    require "./bibliotecas/PHPMailer/SMTP.php";
 
-     public function __get($atributo){
-         return $this->$atributo;
-     }
-     public function __set($atributo, $valor){
-        return $this->$atributo = $valor;
-     }
-     public function mensagemValida(){
-        if(empty($this->para) || empty($this->assunto) || empty($this->mensagem) ){
-             return false;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo){
+            return $this->$atributo;
         }
-        return true;
-     }
-}
+        public function __set($atributo, $valor){
+            return $this->$atributo = $valor;
+        }
+        public function mensagemValida(){
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem) ){
+                return false;
+            }
+            return true;
+        }
+    }
 
 
     $mensagem = new Mensagem();
@@ -30,10 +40,45 @@ class Mensagem {
 #print_r($mensagem);
 
 
-    if($mensagem->mensagemValida()){
-        echo "mensagem valida";
-    }else{
-        echo "mensagem não é validade";
+    if(!$mensagem->mensagemValida()){
+        echo "mensagem não é validada";
+        die();
+    }
+
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'hostappsendmail@gmail.com';                 // SMTP username
+        $mail->Password = 'w&wlHkEpd9';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+    
+        //Recipients
+        $mail->setFrom('hostappsendmail@gmail.com', 'Web completo rementente');
+        $mail->addAddress('hostappsendmail@gmail.com', 'Web completo Destinatario');     // Add a recipient
+        // $mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'oi eu sou o assunto';
+        $mail->Body    = 'oi eu sou o conteudo do <strong>email</strong>';
+        $mail->AltBody = 'oi eu sou o conteudo do email';
+    
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Não foi possível enviar este e-mail! Por favor tente mais tarde';
+        echo 'Detalhes do erro ' . $mail->ErrorInfo;
     }
 
 
